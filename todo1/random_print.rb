918f66834a1202dbfa90db19182a2acb5c68b1d5
Nikki Thean <nikki.thean@gmail.com>
2013-11-12 13:02:39 -0500

    Improve random_print.rb to include Uzo's print file

--- todo1/random_print.rb
+++ todo1/random_print.rb
@@ -1,4 +1,4 @@
-name_files = ["printname_nikki", "write_seans_name", "chris"]
+name_files = ["printname_nikki", "write_seans_name", "chris", "uzo"]
 name = name_files.sample
 
 require_relative(name)
