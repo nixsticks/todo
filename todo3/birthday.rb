45a71d9b2f6aeb5a71487a093db5affd38249a6e
Nikki Thean <nikki.thean@gmail.com>
2013-11-14 09:18:29 -0500

    Improve birthday.rb to make it more modular

--- todo3/birthday.rb
+++ todo3/birthday.rb
@@ -1,3 +1,17 @@
-puts "Tell me the date of your birthday!"
-birthday = Date.parse(gets.chomp).strftime("%m%d")
-puts Date.today.strftime("%m%d") == birthday ? "Happy birthday!" : "Oh."
+class Birthday
+  def get_birthday
+    puts "Tell me the date of your birthday!"
+    Date.parse(gets.chomp).strftime("%m%d")
+  end
+
+  def get_date
+    Date.today.strftime("%m%d")
+  end
+
+  def is_today?
+    puts get_birthday == get_date ? "Happy birthday!" : "Oh."
+  end
+end
+
+birthday = Birthday.new
+birthday.is_today?
