2004ce962caa4320a03f5b4dc7140b8adca70b14
Nikki Thean <nikki.thean@gmail.com>
2013-11-15 09:11:48 -0500

    Add my solution to count_sentences

---
+++ todo4/count_sentences.rb
@@ -0,0 +1,9 @@
+# Write a method on String called `count_sentences` that returns the number of
+# sentences in the string it is called on
+
+class String
+  def count_sentences
+    sentences = /[.,!?]/ =~ self
+    sentences == nil ? 0 : sentences
+  end
+end
