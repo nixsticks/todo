ad21dccd2e62f5f1a9c0c7e80dfa39e428a82993
Nikki Thean <nikki.thean@gmail.com>
2013-11-18 13:16:08 -0500

    Refactor fizzbuzz

--- Loops/fizzbuzz/fizzbuzz.rb
+++ Loops/fizzbuzz/fizzbuzz.rb
@@ -6,4 +6,12 @@ fizzbuzzes = (1..100).map do |i|
   end
 end
 
-puts fizzbuzzes
+def fizzbuzz(num)
+  if num % 15 == 0 then "fizzbuzz"
+  elsif num % 3 == 0 then "fizz"
+  elsif num % 5 == 0 then "buzz"
+  else num
+  end
+end
+
+puts fizzbuzz(5)
