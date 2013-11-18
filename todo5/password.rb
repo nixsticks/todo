c7ca5d4eb69496eee80982ab4e52fe49eb0569cc
Nikki Thean <nikki.thean@gmail.com>
2013-11-18 10:10:33 -0500

    Add password decoder in todo5

---
+++ todo5/password.rb
@@ -0,0 +1,27 @@
+def encode(password)
+  pass_as_string = password.to_s
+  password_array = pass_as_string.split("_")
+  new_password_array = []
+  new_password_array[0] = password_array.last
+  new_password_array[3] = password_array.first
+  new_password_array[1] = password_array[1]
+  new_password_array.compact!
+  joined_password = new_password_array.join(" ")
+  joined_password.gsub!("e", "blake")
+  joined_password.gsub!(" ", "ashley")
+  joined_password
+end
+
+def decode(joined_password)
+  joined_password.gsub!("ashley", " ")
+  joined_password.gsub!("blake", "e")
+  new_password_array = joined_password.split(" ")
+  password_array = []
+  password_array[1] = new_password_array[1]
+  password_array[0] = new_password_array.last
+  password_array[2] = new_password_array.first
+  password = password_array.compact.join("_")
+  password = password.to_sym
+end
+
+puts decode("intblakernblaketsashleythblakeashleyall")
