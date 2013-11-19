1564b9d15b79551c16e47fa2b6bd1549da3514a8
Nikki Thean <nikki.thean@gmail.com>
2013-11-19 10:12:00 -0500

    Refactor Oliver's magic eight ball game to separate out the printing messages from the logic and write better tests

--- oliver_magiceightball/magicball.rb
+++ oliver_magiceightball/magicball.rb
@@ -1,7 +1,5 @@
 class Magic8
-  attr_accessor :messages
-  def initialize
-    @messages = ["Try again later", 
+  MESSAGES = ["Try again later", 
                   "My sources tell me good things are coming your way", 
                   "Who waketh the Kraken?", 
                   "Bleep Bloop I'm just a computer. Why are you asking me?",
@@ -11,25 +9,35 @@ class Magic8
                   "Poop",
                   "Mayyyyyybeeee?"
                   ]
-  end
 
   def greeting
-    puts "Want to know your fortune? Shake the magic eight ball! Shake you computer to play\n...I was kidding. Type 'yes' to play and 'no' to quit"
-    return gets.chomp.downcase
+    "Want to know your fortune? Shake the magic eight ball! Shake you computer to play\n...I was kidding. Type 'yes' to play and 'no' to quit"
   end
 
-  def shake(input)
-    if input == 'yes'
-      @messages.sample
-    elsif input=='no'
+  def get_input
+    gets.chomp.downcase
+  end
+
+  def shake(get_input)
+    if get_input == 'yes'
+      MESSAGES.sample
+    elsif get_input=='no'
       "Whatever. I didn't want to tell your fortune anyway."
     else
       "I didn't understand that. Type 'yes' to play or 'no' to quit"
     end
   end
+
+  def run
+    puts greeting
+    # puts shake(get_input)
+  end
 end
 
+
+
 # new_game = Magic8.new
+# new_game.run
 
 
 # user_input = new_game.greeting
--- oliver_magiceightball/magicball_spec.rb
+++ oliver_magiceightball/magicball_spec.rb
@@ -13,18 +13,10 @@ describe "Magic8" do
                   "Mayyyyyybeeee?"
                   ]
 
-  describe "#initialize" do
-    it "should create an array of messages" do
-      expect(game.messages).to be_an_instance_of(Array)
-      expect(game.messages).to include("Who waketh the Kraken?")
-      expect(game.messages).to include("STOP SHAKING ME, CALM DOWN!")
-    end
-  end
-
   describe "#greeting" do
     it "should ask me if I want to know my fortune" do
-      game.stub(:gets => "yes\n")
-      game.should_receive(:puts).with("Want to know your fortune? Shake the magic eight ball! Shake you computer to play\n...I was kidding. Type 'yes' to play and 'no' to quit")
+      game.should_receive(:puts).with("Want to know your fortune? Shake the magic eight ball! Shake you computer to play\n...I was kidding. Type 'yes' to play and 'no' to quit").and_call_original
+      game.run 
     end
   end
 
