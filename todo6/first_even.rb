8e439239c3a62c8fce0fee956d8643434cab08f8
Nikki Thean <nikki.thean@gmail.com>
2013-11-19 09:25:33 -0500

    Changed first_even.rb to not use detect

--- todo6/first_even.rb
+++ todo6/first_even.rb
@@ -1,3 +1,14 @@
+# But this one is so much better!!!
+
+# def first_even(items)
+#   items.detect(&:even?)
+# end
+
 def first_even(items)
-  items.detect(&:even?)
+  items.each do |x|
+    if x.even?
+      return x
+      exit
+    end
+  end
 end
