7c2b9034936e05d9db63239cd2ba3af75bb3565e
Nikki Thean <nikki.thean@gmail.com>
2013-11-18 20:10:55 -0500

    Removed test p from apple_picker.rb

--- holiday_labs/apple_picker.rb
+++ holiday_labs/apple_picker.rb
@@ -4,20 +4,14 @@ def apple_picker(array)
   apples
 end
 
-p apple_picker(["apple", "orange", "apple"])
-
 def apple_picker_collect(array)
   apples = array.collect {|item| item == "apple" ? item : next}
   apples.compact!
 end
 
-p apple_picker_collect(["apple", "orange", "apple"])
-
 def apple_picker_select(array)
   apples = array.select {|item| item == "apple"}
 end
-
-p apple_picker_select(["apple", "orange", "apple"])
 
 # 'Collect' returns a new array made up of the result of each item of the original array being passed to the block.
 # 'Select', on the other hand, returns a new array made up of each item for which the block returns true.
