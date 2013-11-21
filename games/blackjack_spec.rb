41c7aa680592a21674d4ce498edc60e168e0c6d2
Nikki Thean <nikki.thean@gmail.com>
2013-11-21 16:43:17 -0500

    Add blackjack spec - tentative

--- games/blackjack_spec.rb
+++ games/blackjack_spec.rb
@@ -2,9 +2,11 @@ require './blackjack.rb'
 
 describe "Blackjack" do
   let(:game) {Blackjack.new}
-  describe "#start_game" do
+  describe "#run" do
     it "should welcome the user to the casino" do
+      game.stub(:gets) {"y\n"}
       game.should_receive(:puts).with("\nWelcome to my casino! \nWould you like to play a game of Blackjack?").and_call_original
+      game.should_receive(:puts).with("\nHave a seat!\n\nDrawing up a chair...\n\n").and_call_original
       game.run
     end
   end
