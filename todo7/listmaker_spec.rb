fd015f263d764198377db80105c77b4c7faad2bc
Nikki Thean <nikki.thean@gmail.com>
2013-11-21 16:43:31 -0500

    Add listmaker spec

--- todo7/listmaker_spec.rb
+++ todo7/listmaker_spec.rb
@@ -1,6 +1,6 @@
 require './listmaker'
 
-describe 'Array, #make_list' do
+describe Array, '#make_list' do
   it 'should return an array of strings in a numbered list' do
     expect(["hello", "my", "name", "is", "nikki"].make_list).to eq(["1. hello", "2. my", "3. name", "4. is", "5. nikki"])
     expect([99, 88, 77, 66].make_list).to eq(["1. 99", "2. 88", "3. 77", "4. 66"])
