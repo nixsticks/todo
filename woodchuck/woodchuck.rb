c5242061d959bca95de3d8341c9ebb667a2c43d5
Nikki Thean <nikki.thean@gmail.com>
2013-11-26 15:28:41 -0500

    Add method outside class as well as class methods to make random woodchucks... whichever you want

--- woodchuck/woodchuck.rb
+++ woodchuck/woodchuck.rb
@@ -18,27 +18,47 @@ class Woodchuck
     @@woodchuck_count
   end
 
-  def self.create_random_woodchucks
-    rand(10).times{ Woodchuck.new }
-  end
+  # def self.create_random_woodchucks
+  #   rand(10).times{ Woodchuck.new }
+  # end
 
-  def self.all_chuck_wood
-    WOODCHUCKS.each do |woodchuck|
-      rand(10).times {woodchuck.chuck_wood}
-    end
-  end
+  # def self.all_chuck_wood
+  #   WOODCHUCKS.each do |woodchuck|
+  #     rand(10).times {woodchuck.chuck_wood}
+  #   end
+  # end
 
-  def self.all_wood_chucked
-    count = 0
-    WOODCHUCKS.each {|woodchuck| count += woodchuck.chuck_count}
-    count
-  end
+  # def self.all_wood_chucked
+  #   count = 0
+  #   WOODCHUCKS.each {|woodchuck| count += woodchuck.chuck_count}
+  #   count
+  # end
 end
 
+def create_random_woodchucks
+  rand(1..10).times{ Woodchuck.new }
+end
 
-Woodchuck.create_random_woodchucks
-Woodchuck.all_chuck_wood
-puts Woodchuck.all_wood_chucked
+def all_chuck_wood
+  Woodchuck::WOODCHUCKS.each do |woodchuck|
+      rand(1..10).times {woodchuck.chuck_wood}
+    end
+end
+
+def all_wood_chucked
+  count = 0
+  Woodchuck::WOODCHUCKS.each {|woodchuck| count += woodchuck.chuck_count}
+  count
+end
+
+create_random_woodchucks
+all_chuck_wood
+puts all_wood_chucked
+
+
+# Woodchuck.create_random_woodchucks
+# Woodchuck.all_chuck_wood
+# puts Woodchuck.all_wood_chucked
 
 # using this class:
 # - create a random number of woodchucks
