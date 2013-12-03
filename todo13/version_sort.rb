30d036ba4a569bc2fd7a7359b60529c1335e7327
Nikki Thean <nikki.thean@gmail.com>
2013-12-03 13:40:18 -0500

    Add method that uses singleton classes

--- todo13/version_sort.rb
+++ todo13/version_sort.rb
@@ -1,76 +1,46 @@
 class Array
   def version_sort
-    String.send(:attr_accessor, :num1, :num2, :letter, :decimals)
-
     each do |element|
+      element.singleton_class.send(:attr_accessor, :num1, :num2, :letter, :decimals)
+
       number = /\-(?<first_number>\d+)\.?(?<second_number>[\d\.]+)?(?<letter_version>[a-z])?\.[a-z]/.match(element)
       decimals = element.split("").select {|letter| letter == "."}.size
 
-      element.num1 = number[:first_number].to_i
+      element.send(:num1=, number[:first_number].to_i)
 
-      number[:second_number] ? element.num2 = number[:second_number].to_f : element.num2 = 0.0
+      number[:second_number] ? element.send(:num2=, number[:second_number].to_f) : element.send(:num2=, 0.0)
 
-      number[:letter_version] ? element.letter = number[:letter_version] : element.letter = "0"
+      number[:letter_version] ? element.send(:letter=, number[:letter_version]) : element.send(:letter=, "0")
 
-      element.decimals = decimals
+      element.send(:decimals=, decimals)
     end
 
     sort! do |a,b|
-      [a.num1, a.num2, a.decimals, a.letter] <=> [b.num1, b.num2, b.decimals, b.letter]
+      [a.send(:num1), a.send(:num2), a.send(:decimals), a.send(:letter)] <=> [b.send(:num1), b.send(:num2), b.send(:decimals), b.send(:letter)]
     end
   end
 end
 
+# SIMPLER METHOD BUT MONKEYPATCHES STRING CLASS SOMEWHAT:
 
-filenames = [
-  "foo-1.10.2.ext",
-  "foo-1.11.ext",
-  "foo-1.3.ext",
-  "foo-1.50.ext",
-  "foo-1.8.7.ext",
-  "foo-1.9.3.ext",
-  "foo-1.ext",
-  "foo-10.1.ext",
-  "foo-10.ext",
-  "foo-100.ext",
-  "foo-13.ext",
-  "foo-2.0.0.ext",
-  "foo-2.0.1.ext",
-  "foo-2.0.ext",
-  "foo-2.007.ext",
-  "foo-2.01.ext",
-  "foo-2.012b.ext",
-  "foo-2.01a.ext",
-  "foo-2.0a.ext",
-  "foo-2.0b.ext",
-  "foo-2.1.ext",
-  "foo-25.ext",
-  "foo-6.ext",
-]
-version_sorted_filenames = [
-  "foo-1.ext",
-  "foo-1.3.ext",
-  "foo-1.8.7.ext",
-  "foo-1.9.3.ext",
-  "foo-1.10.2.ext",
-  "foo-1.11.ext",
-  "foo-1.50.ext",
-  "foo-2.0.ext",
-  "foo-2.0a.ext",
-  "foo-2.0b.ext",
-  "foo-2.0.0.ext",
-  "foo-2.0.1.ext",
-  "foo-2.01.ext",
-  "foo-2.1.ext",
-  "foo-2.01a.ext",
-  "foo-2.007.ext",
-  "foo-2.012b.ext",
-  "foo-6.ext",
-  "foo-10.ext",
-  "foo-10.1.ext",
-  "foo-13.ext",
-  "foo-25.ext",
-  "foo-100.ext",
-]
+# class Array
+#   def version_sort
+#     String.send(:attr_accessor, :num1, :num2, :letter, :decimals)
+#     each do |element|
+#       number = /\-(?<first_number>\d+)\.?(?<second_number>[\d\.]+)?(?<letter_version>[a-z])?\.[a-z]/.match(element)
+#       decimals = element.split("").select {|letter| letter == "."}.size
 
-# p filenames.version_sort
+#       element.num1 = number[:first_number].to_i
+
+#       number[:second_number] ? element.num2 = number[:second_number].to_f : element.num2 = 0.0
+
+#       number[:letter_version] ? element.letter = number[:letter_version] : element.letter = "0"
+
+#       element.decimals = decimals
+#     end
+
+#     sort! do |a,b|
+#       [a.num1, a.num2, a.decimals, a.letter] <=> [b.num1, b.num2, b.decimals, b.letter]
+#     end
+#   end
+# end
