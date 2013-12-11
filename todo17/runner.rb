c6d0f9a98aef712111ab3495f7cd91bc2b0ef670
Nikki Thean <nikki.thean@gmail.com>
2013-12-11 09:38:40 -0500

    Initial commit

---
+++ todo17/runner.rb
@@ -0,0 +1,36 @@
+require_relative './jukebox'
+require_relative './song_library'
+
+def run
+  puts "Welcome to Ruby Console Jukebox!"
+  puts "Enter a command to continue. Type 'help' for a list of commands."
+  command = get_command
+  while command != "exit" do
+    run_command(command) unless command.downcase == "exit"
+    command = get_command
+  end
+end
+
+def get_command
+  gets.strip
+end
+
+def run_command(command)
+  case command
+  when "help"
+    show_help
+  else
+    jukebox(command)
+  end
+end
+
+def show_help
+  help = "Never worked a jukebox, eh? Pretty standard. Available commands are:
+          'help' - shows this menu
+          'list' - lists the whole song library
+          or you can enter an artist's name to show that artist's songs
+          or you can enter an artist's name to show that artist's songs."
+  puts help
+end
+
+run
