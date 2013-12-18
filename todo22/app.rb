331c1a5e69efa0e62ffdbddf88222f693d55436f
Nikki Thean <nikki.thean@gmail.com>
2013-12-18 09:20:48 -0500

    Add more holidays

--- todo22/app.rb
+++ todo22/app.rb
@@ -3,19 +3,34 @@ Bundler.require
 
 module Calendar
   class App < Sinatra::Application
+    set :today, Time.now.strftime("%m/%d")
+    set :holidays, {
+      :birthday => "07/31",
+      :christmas => "12/25",
+      :new_year => "01/01",
+      :halloween => "10/31"
+    }
+
     get '/birthday' do
       birthday = Time.new(1987, 12, 18).strftime("%m/%d")
-      today = Time.now.strftime("%m/%d")
-      if birthday == today
+      if birthday == settings.today
         erb :yes
       else
         erb :no
       end
     end
 
-    helpers do
-      def sameday
-        
+    get '/:event' do
+      event = params[:event]
+      @event = settings.holidays[event.to_sym]
+      if @event
+        if @event == settings.today
+          erb :yes
+        else
+          erb :no
+        end
+      else
+        erb :not_found
       end
     end
   end
