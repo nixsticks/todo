fcb01cd05e390d638a5fcc015204eff329381422
Nikki Thean <nikki.thean@gmail.com>
2013-12-18 09:10:08 -0500

    Complete first part of assignment

--- todo22/app.rb
+++ todo22/app.rb
@@ -3,7 +3,14 @@ Bundler.require
 
 module Calendar
   class App < Sinatra::Application
-    get '/' do
+    get '/birthday' do
+      birthday = Time.new(1987, 07, 31)
+      today = Time.now
+      if birthday.month == today.month && birthday.day == today.day
+        erb :yes
+      else
+        erb :no
+      end
     end
   end
 end
