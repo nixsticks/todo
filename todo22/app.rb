c2cab4a1953e993c194d8318e9a433b33d16a688
Nikki Thean <nikki.thean@gmail.com>
2013-12-18 09:13:16 -0500

    Update solution

--- todo22/app.rb
+++ todo22/app.rb
@@ -4,12 +4,18 @@ Bundler.require
 module Calendar
   class App < Sinatra::Application
     get '/birthday' do
-      birthday = Time.new(1987, 07, 31)
-      today = Time.now
-      if birthday.month == today.month && birthday.day == today.day
+      birthday = Time.new(1987, 12, 18).strftime("%m/%d")
+      today = Time.now.strftime("%m/%d")
+      if birthday == today
         erb :yes
       else
         erb :no
+      end
+    end
+
+    helpers do
+      def sameday
+        
       end
     end
   end
