ace1e8a599cc5379a873437e158d46cbb4902f45
Nikki Thean <nikki.thean@gmail.com>
2013-12-19 09:29:19 -0500

    Complete assignment

---
+++ todo23/fake_array.rb
@@ -0,0 +1,20 @@
+class FakeArray
+  attr_accessor :elements
+
+  def initialize(array)
+    @elements = array
+  end
+
+  def each(&block)
+    block ? elements.each {|element| yield element} : elements.to_enum
+  end
+ 
+  def first
+    elements.first
+  end
+ 
+  def [](i)
+    elements[i]
+  end
+end
+ 
