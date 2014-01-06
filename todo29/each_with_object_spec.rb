c59ebf96dc7d0d8a780f81491e1f59ff37060f45
Nikki Thean <nikki.thean@gmail.com>
2014-01-06 08:58:18 -0500

    Add even more tests

--- todo29/each_with_object_spec.rb
+++ todo29/each_with_object_spec.rb
@@ -2,7 +2,8 @@ require './each_with_object'
 
 describe '#even_sum' do
   it 'should return an array containing one reversed string for each even string' do
-    expected = ["drib", "hsif"]
-    expect(even_sum(["cat", "dog", "bird", "fish"])).to eq(expected)
+    expect(even_sum(["cat", "dog", "bird", "fish"])).to eq(["drib", "hsif"])
+    expect(even_sum(["hello", "goodbye", "nikki"])).to eq([])
+    expect(even_sum(["oh", "my", "goodness"])).to eq(["ho", "ym", "ssendoog"])
   end
 end
