881da2adc9b9a6d3d8e44484b0169ca55f2401f8
Nikki Thean <nikki.thean@gmail.com>
2014-01-08 10:09:34 -0500

    Complete assignment

---
+++ todo31/palindrome.rb
@@ -0,0 +1,19 @@
+require 'debugger'
+
+def longest_palindrome(string)
+  counter = string.length - 1
+  split = string.split("")
+  palindromes = []
+
+  while counter > 0
+    array = split.each_cons(counter).to_a
+    array.each do |inner_array|
+      i = inner_array.size
+      j = inner_array.size/2
+      return inner_array.join if inner_array[0..(j-1)] == inner_array[j..i].reverse
+    end
+    counter -= 2
+  end
+end
+
+#each_cons even sizes. each slice half. see if half
---
+++ todo31/palindrome_spec.rb
@@ -0,0 +1,7 @@
+require './palindrome'
+
+describe '#longest_palindrome' do
+  it 'should return the longest palindrome in a given string' do
+    expect(longest_palindrome("afbbbfjdjklgdfdhfdkjfffhhfffjkdfhdhkyejejfjkd")).to eq("dhfdkjfffhhfffjkdfhd")
+  end
+end
