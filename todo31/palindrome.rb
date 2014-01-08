3913b8337063e27df453ccee3bf19a4898563e3e
Nikki Thean <nikki.thean@gmail.com>
2014-01-08 12:01:58 -0500

    Update bc I realized I stupid

--- todo31/palindrome.rb
+++ todo31/palindrome.rb
@@ -1,15 +1,11 @@
 def longest_palindrome(string)
-  i = counter(string.length)
+  counter = string.length
 
-  while i > 0
-    array = string.split("").each_cons(i).to_a
+  while counter > 0
+    array = string.split("").each_cons(counter).to_a
     array.each {|inner| return inner.join if palindrome?(inner)}
-    i -= 2
+    counter -= 1
   end
-end
-
-def counter(i)
-  i % 2 == 0 ? i : (i - 1)
 end
 
 def string_to_array(string)
@@ -17,5 +13,9 @@ def string_to_array(string)
 end
 
 def palindrome?(array)
-  array[0...array.size/2] == array[array.size/2..array.size].reverse
+  if array.size % 2 == 0
+    array[0...array.size/2] == array[array.size/2..array.size].reverse
+ else
+    array[0...array.size/2] == array[(array.size/2) + 1..array.size].reverse
+ end
 end
--- todo31/palindrome_spec.rb
+++ todo31/palindrome_spec.rb
@@ -3,5 +3,6 @@ require './palindrome'
 describe '#longest_palindrome' do
   it 'should return the longest palindrome in a given string' do
     expect(longest_palindrome("afbbbfjdjklgdfdhfdkjfffhhfffjkdfhdhkyejejfjkd")).to eq("dhfdkjfffhhfffjkdfhd")
+    expect(longest_palindrome("abbobsdfa")).to eq("bob")
   end
 end
