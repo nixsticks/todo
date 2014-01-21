cd32fc8a72eb70d1cb2f5aa1498f8d5b1e79d797
Nikki Thean <nikki.thean@gmail.com>
2014-01-21 09:31:48 -0500

    Add test

--- todo41/spec/fizzBuzzSpec.js
+++ todo41/spec/fizzBuzzSpec.js
@@ -1,9 +1,5 @@
 describe( "#fizzBuzz", function() {
-  it("should return the number of sentences in a string", function() {
-    expect(countSentences("one. two. three?")).toBe(3);
-  });      
-
-  it("should return zero if there are no sentences in a string", function() {
-    expect(countSentences("")).toBe(0);
-  });   
+  it("should say fizz for numbers divisible by 3, buzz for numbers divisible by 5, and fizzbuzz for numbers divisible by 3 and 5", function() {
+    expect(fizzBuzz(15)).toEqual([1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz"]);
+  });
 });
--- todo41/src/fizzBuzz.js
+++ todo41/src/fizzBuzz.js
@@ -1,16 +1,45 @@
-function fizzBuzz() {
-  for (var i = 0; i < 101; i++) {
+// simple version, no test.
+
+// function fizzBuzz() {
+//   for (var i = 1; i < 101; i++) {
+//     if (i % 15 === 0) {
+//       console.log("fizzbuzz");
+//     }
+//     else if (i % 5 === 0) {
+//       console.log("buzz");
+//     }
+//     else if (i % 3 === 0) {
+//       console.log("fizz");
+//     }
+//     else {
+//       console.log(i);
+//     }
+//   }
+// }
+
+function fizzBuzz(n) {
+  var array = [];
+
+  for (var i = 1; i <= n; i++) {
     if (i % 15 === 0) {
-      console.log("fizzbuzz");
+      array.push("fizzbuzz");
     }
     else if (i % 5 === 0) {
-      console.log("buzz");
+      array.push("buzz");
     }
     else if (i % 3 === 0) {
-      console.log("fizz");
+      array.push("fizz");
     }
     else {
-      console.log(i);
+      array.push(i);
     }
   }
+
+  return array;
+}
+
+function print(array) {
+  array.forEach(function(element) {
+    console.log(element);
+  });
 }
